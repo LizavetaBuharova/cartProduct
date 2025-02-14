76d1ad7f203c439eb47c828428571c4e5396a4e9
Buharova Elizaveta <byharova.077@gmail.com>
2025-02-15 01:33:49 +0300

    add operation

--- vite-project/src/buttomNum.js
+++ vite-project/src/buttomNum.js
@@ -1,32 +1,11 @@
+import { operation } from "./operation";
+
 export default function buttonMenu() {
-
-  let num = document.querySelector(".count");
-  let nameGoods = document.querySelector(".name-goods");
-  let countPrice = document.querySelector(".count-price");
-  let finalPrice = document.querySelector(".final-price");
-  let newPrice = document.querySelector(".new-price");
-  let plus = document.querySelector(".plus");
-  let minus = document.querySelector(".minus");
-  let nameProduct = document.querySelector(".main-h1");
-
-  plus.addEventListener("click", () => {
-    ++num.innerHTML;
-    nameGoods.innerHTML = nameProduct.innerHTML;
-    const textValue = newPrice.innerHTML;
-    const numericString = textValue.replace("$", "");
-    countPrice.innerHTML = "$" + (+numericString * +num.innerHTML).toFixed(2);
-    finalPrice.innerHTML = countPrice.innerHTML;
+  document.querySelector(".plus").addEventListener("click", () => {
+    operation("+");
   });
 
-  minus.addEventListener("click", () => {
-    --num.innerHTML;
-    const textValue = newPrice.innerHTML;
-    const numericString = textValue.replace("$", "");
-    countPrice.innerHTML = "$" + (+numericString * +num.innerHTML).toFixed(2);
-    finalPrice.innerHTML = countPrice.innerHTML;
-    if (num.innerHTML == 0) {
-        nameGoods.innerHTML = "Nothing";
-    } 
+  document.querySelector(".minus").addEventListener("click", () => {
+    operation("-");
   });
-
 }
--- vite-project/src/operation.js
+++ vite-project/src/operation.js
@@ -1,19 +1,23 @@
-export function plus() {
+export function operation(str) {
   let num = document.querySelector(".count");
   let nameGoods = document.querySelector(".name-goods");
   let countPrice = document.querySelector(".count-price");
   let finalPrice = document.querySelector(".final-price");
   let newPrice = document.querySelector(".new-price");
-  let plus = document.querySelector(".plus");
-  let minus = document.querySelector(".minus");
   let nameProduct = document.querySelector(".main-h1");
 
-  () => {
+  if (str == "+") {
     ++num.innerHTML;
-    nameGoods.innerHTML = nameProduct.innerHTML;
-    const textValue = newPrice.innerHTML;
+  } if (str == "-") {
+    --num.innerHTML;
+  }
+
+  nameGoods.innerHTML = nameProduct.innerHTML;
+  const textValue = newPrice.innerHTML;
     const numericString = textValue.replace("$", "");
     countPrice.innerHTML = "$" + (+numericString * +num.innerHTML).toFixed(2);
     finalPrice.innerHTML = countPrice.innerHTML;
-  }
+    if (num.innerHTML == 0) {
+        nameGoods.innerHTML = "Nothing";
+    } 
 }
